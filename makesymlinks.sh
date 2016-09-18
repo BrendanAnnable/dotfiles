8840859af53af8c8216307ebb7cc6845b45b003c
Brendan Annable <brendan@annable.id.au>
2016-09-19 01:14:47 +1000

    Disable dotfile backups

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,9 +7,9 @@ olddir="$HOME/dotfiles/old"
 files="bash_profile gitignore_global zpreztorc zshrc gitconfig ideavimrc vimrc zprofile dir_colors ackrc spacemacs"
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p "$olddir"
-echo "done"
+# echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+# mkdir -p "$olddir"
+# echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
@@ -18,8 +18,8 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from $HOME to $olddir"
-    mv "$HOME/.$file" "$olddir/"
+    # echo "Moving any existing dotfiles from $HOME to $olddir"
+    # mv "$HOME/.$file" "$olddir/"
     echo "Creating symlink to $file in home directory."
     ln -s "$dir/$file" "$HOME/.$file"
 done
